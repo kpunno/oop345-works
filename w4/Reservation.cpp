532f2dbe8a80c482df979d7455ceaf115431d38a
kpunno <k.punno@gmail.com>
2022-10-03 09:42:56 -0400

    reservation.h / reservation.cpp

---
+++ w4/Reservation.cpp
@@ -0,0 +1,17 @@
+/*
+Name ---- Kristjan Punno
+Email --- kpunno@myseneca.ca
+ID ------ 150695211
+Section - NCC
+Date ---- 2022-09-21
++----------------------------------------------------------------------+
+|  I have done all the coding by myself and only copied the code that  |
+|  my professor provided to complete my workshops and assignments.     |
++----------------------------------------------------------------------+
+*/
+
+#include "Reservation.h"
+
+namespace sdds {
+
+}
---
+++ w4/Reservation.h
@@ -0,0 +1,27 @@
+/*
+Name ---- Kristjan Punno
+Email --- kpunno@myseneca.ca
+ID ------ 150695211
+Section - NCC
+Date ---- 2022-09-21
++----------------------------------------------------------------------+
+|  I have done all the coding by myself and only copied the code that  |
+|  my professor provided to complete my workshops and assignments.     |
++----------------------------------------------------------------------+
+*/
+
+
+#ifndef RESERVATION_H_
+#define RESERVATION_H_
+
+namespace sdds {
+
+   class Reservation
+   {
+
+   };
+
+}
+
+#endif // RESERVATION_H_
+
