b935c58f75de1d2c3faa97a0948832e4bc94e80c
kpunno <k.punno@gmail.com>
2022-11-06 23:48:38 -0500

    documentation and write-up

--- w6/w6_p2.cpp
+++ w6/w6_p2.cpp
@@ -8,7 +8,7 @@ Email-- - kpunno@myseneca.ca
 ID------ 150695211
 Section - NCC
 Date---- 2022 - 11 - 06
-+ ---------------------------------------------------------------------- +
++---------------------------------------------------------------------- +
 | I have done all the coding by myself and only copied the code that  |
 |  my professor provided to complete my workshops and assignments. |
 +---------------------------------------------------------------------- +
