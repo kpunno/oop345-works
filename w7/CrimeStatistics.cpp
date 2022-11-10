90331cd847a06f06a01bbe7d19e7dfd9acb03d5c
kpunno <k.punno@gmail.com>
2022-11-10 11:10:37 -0500

    w7p1 functional

--- w7/CrimeStatistics.cpp
+++ w7/CrimeStatistics.cpp
@@ -1,22 +1,81 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include "CrimeStatistics.h"
 
 namespace sdds {
 
    CrimeStatistics::CrimeStatistics(const char* filename) {
-      std::ifstream file;
-      file.open(filename);
+      std::ifstream file(filename);
 
-      if (file.is_open()) {
-
+      if (!file) {
+         std::cerr << "File does not exist!" << std::endl;
       }
-      
+      while (file) {
+         std::string line;
+         std::getline(file, line);
+
+         if (line.length()) {
+            Crime crime;
+
+            crime.m_province = splice(line, 25);
+            crime.m_district = splice(line, 25);
+            crime.m_crime = splice(line, 25);
+
+            try {
+               crime.m_year = std::stoi(splice(line, 5));
+               crime.m_noCases = std::stoi(splice(line, 5));
+               crime.m_noResolved = std::stoi(splice(line, 5));
+
+               m_crimes.push_back(crime);
+            }
+            catch (...) {
+               std::cerr << "Invalid record!" << std::endl;
+            }
+         }
+      }
    }
 
-   void CrimeStatistics::display(std::ostream& out) const {
-      ;
+   auto CrimeStatistics::display(std::ostream& out)->void const {
+      for (auto it = m_crimes.begin(); it != m_crimes.end(); ++it) {
+         out << *it << std::endl;
+      }
+   }
+
+   auto operator<<(std::ostream& out, const Crime& crime)->std::ostream& {
+      out << std::left << "| ";
+      out << std::setw(21) << crime.m_province << " | ";
+      out << std::setw(15) << crime.m_district << " | ";
+      out << std::setw(20) << crime.m_crime << " | ";
+      out << std::right;
+      out << std::setw(6) << crime.m_year << " | ";
+      out << std::setw(4) << crime.m_noCases << " | ";
+      out << std::setw(3) << crime.m_noResolved << " |";
+
+      return out;
+   }
+
+   auto splice(std::string& line, size_t length)->std::string {
+      std::string output = line.substr(0, length);
+      eraseWhiteSpace(output);
+      line.erase(0, length);
+      return output;
+   }
+
+   auto eraseWhiteSpace(std::string& str)->void {
+      unsigned int i = 0;
+      if (str != "") {
+         while (str[i] == ' ') {
+            str.erase(i, 1);
+         }
+         if (str.length() != 0) {
+            i = str.length() - 1;
+            while (str[i] == ' ') {
+               str.erase(i--, 1);
+            }
+         }
+      }
    }
 
 }
--- w7/CrimeStatistics.h
+++ w7/CrimeStatistics.h
@@ -2,28 +2,40 @@
 #ifndef CRIME_STATISTICS_H_
 #define CRIME_STATISTICS_H_
 
+#include <vector>
 #include <iostream>
 #include <string>
 
 namespace sdds {
 
    struct Crime {
-      std::string province;
-      std::string district;
-      std::string crime;
-      size_t noCases;
-      unsigned int year;
-      size_t noResolved;
+      std::string m_province;
+      std::string m_district;
+      std::string m_crime;
+      unsigned int m_year;
+      size_t m_noCases;
+      size_t m_noResolved;
    };
 
    class CrimeStatistics {
-      Crime* m_crime{};
+      std::vector<Crime> m_crimes{};
 
    public:
+      // construct accepts filename and supplies m_crimes with a collection of crimes
       CrimeStatistics(const char*);
-      void display(std::ostream& out) const;
 
+      // iterates through the collection, inserting each crime into ostream
+      auto display(std::ostream& out)->void const;
    };
+
+   // splices a string, returning the substring, and erasing the extracted content from line
+   std::string splice(std::string&, size_t);
+
+   // trims trailing and leading whitespace
+   auto eraseWhiteSpace(std::string&)->void;
+
+   // inserts a crime into ostream
+   auto operator<<(std::ostream& out, const Crime& crime)->std::ostream&;
 }
 
 #endif // CRIME_STATISTICS_H_
