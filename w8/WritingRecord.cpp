f683b822a770db3e03620dff12d32fb009bb5734
kpunno <k.punno@gmail.com>
2022-11-17 18:10:15 -0500

    w8p1 started

---
+++ w8/WritingRecord.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include "GeneratingList.h"
+#include "EmpProfile.h"
+#include "WritingRecord.h"
+
+using namespace std;
+
+namespace sdds {
+	GeneratingList<EmployeeWage> writeRaw(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal) {
+		GeneratingList<EmployeeWage> activeEmp;
+		
+		// iterate through emp (containing names)
+		for (size_t i{ 0 }; i < emp.size(); i++) {
+
+			// for each employee iterate through sal ...
+			for (size_t j{ 0 }; j < sal.size(); j++) {
+
+				// if a match is found between a 'name' and a 'salary'
+				if (emp[i].id == sal[j].id) {
+
+					// if the SIN is valid
+					if (activeEmp.luhnAlgo(emp[i].id)) {
+
+						// ptr points to dynamically allocated EmployeeWage
+						EmployeeWage* ptr = new EmployeeWage{ emp[i].name, sal[j].salary };
+
+						// validate the salary as well
+						ptr->rangeValidator();
+						activeEmp += ptr;
+					}
+				}
+			}
+		}
+		
+		return activeEmp;
+	}
+}
